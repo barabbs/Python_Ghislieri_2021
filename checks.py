760a4e56c68180eaf0b2b259d4e902c913b0ec49
Daniel Di Labio <you@example.com>
2021-12-02 21:48:06 +0100

    Abbiamo aggiunto funzione (Aggiungi al Calendario)

--- checks.py
+++ checks.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 
-def Controllo(FasciaOraria, Calendario):
-    inizio=FasciaOraria[0]
-    fine=FasciaOraria[1]
+def Controllo(inizio, fine, Calendario):
     for Event in Calendario:
         if not (fine<=Event[0] or inizio>=Event[1]):
             return False
@@ -10,6 +8,17 @@ def Controllo(FasciaOraria, Calendario):
 
 inizio=datetime(2020,1,13)
 fine=datetime(2020,1,16)
-Calendario=[(datetime(2020,1,14),datetime(2020,1,15),"Pippo", "motivo")]
+Calendario=[(datetime(2020,1,12),datetime(2020,1,17),"Pippo", "motivo")]
 
-print(Controllo((inizio,fine), Calendario))
+print(Controllo(inizio, fine, Calendario))
+
+def Aggiungi(inizio, fine, nome, motivo, Calendario):
+     if Controllo(inizio, fine, Calendario)==False:
+         return "Ah che dispiazere"
+     else:
+         Calendario+=[inizio, fine, nome, motivo]
+         Calendario=sorted(Calendario, key=lambda x,y: x[0]<y[0])
+         
+#def Stampa(Calendario):
+    
+    
