3545cc686c843320557e0c6a9c0691f5acbc32a4
Romeo Gessaga <you@example.com>
2021-12-07 22:53:19 +0100

    aggiunto errore orari fuori valore

--- interface.py
+++ interface.py
@@ -6,9 +6,7 @@ scelta = input("cosa vuoi fare?\n a)prenotare campo\n b)controllare orari\n")
 if scelta == "a": 
     print("Per prenotare a cavallo di due giorni, fai due prenotazioni per i due giorni diversi")
     nome = input("come ti chiami?")
-    motivo = input("motivo della prenotazione?")
-    
-        
+    motivo = input("motivo della prenotazione?") 
     x=0
     while x==0:    
         try: 
@@ -20,21 +18,17 @@ if scelta == "a":
             scelta_minuto_inizio = int(input ("minuto inizio")) 
             scelta_ora_fine = int(input ("ora fine")) 
             scelta_minuto_fine = int(input ("minuto fine"))
+            
+            iniziale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_inizio, scelta_minuto_inizio)
+            finale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_fine, scelta_minuto_fine)
            
         except ValueError:
             x=0
-            print("inserisci la data nel formato AAAA-MM-GG e l'ora nel formato HH-MM")
-           
-        
-        
-    iniziale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_inizio, scelta_minuto_inizio)
-    finale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_fine, scelta_minuto_fine)
-    
+            print("inserisci la data nel formato AAAA-MM-GG e l'ora nel formato HH-MM")    
     x = Aggiungi(iniziale, finale, nome, motivo)
     print(x)
     
 if scelta == "b":
-    
     x=0
     while x==0:    
         try: 
@@ -46,18 +40,15 @@ if scelta == "b":
             scelta_minuto_inizio = int(input ("minuto inizio")) 
             scelta_ora_fine = int(input ("ora fine")) 
             scelta_minuto_fine = int(input ("minuto fine"))
-           
+            
+            iniziale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_inizio, scelta_minuto_inizio)
+            finale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_fine, scelta_minuto_fine)
         except ValueError:
             x=0
             print("inserisci la data nel formato AAAA-MM-GG e l'ora nel formato HH-MM")
-        
-        
-    iniziale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_inizio, scelta_minuto_inizio)
-    finale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_fine, scelta_minuto_fine)
-    
     x = Controllo(iniziale, finale)
-   
     if x == True:
         print("è libero")
     else:
         print ("non è libero")
+        
