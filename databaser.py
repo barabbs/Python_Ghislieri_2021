460146bd80cf8ed96826d0529a7d44eabf62f898
Luca Vai <you@example.com>
2021-12-07 22:27:29 +0100

    creato databaser

--- databaser.py
+++ databaser.py
@@ -3,30 +3,30 @@ from datetime import datetime
 
 
 FILENAME = "Prenotazioni.csv"
-FORMAT = "%m/%d/%Y %H:%M:%S"
+FORMAT = "%Y/%m/%d %H:%M:%S"
 
 
+def PrendiCalendario():
+    """restituisce una lista, con elementi le prenotazioni (in formato (datetime-datetime-str-str))"""
+    with open("Prenotazioni.csv", 'r') as csvfile:
+        numrig = 0
+        ans = []
+        for row in csvfile.readlines():
+            if numrig >0 :
+                a = row.strip().split(',')
+                # print(a)
+                a[0] = datetime.strptime(a[0], FORMAT)
+                a[1] = datetime.strptime(a[1], FORMAT)
+                ans.append(tuple(a))
+            numrig += 1
+    return  ans
 
-
-def Creatabella():
-	with open(FILENAME, 'w', newline='') as csvfile:
+def Creatabella(nomecsv):
+	with open(nomecsv, 'w') as csvfile:
 		fieldnames = ['Inizio', 'Fine', 'Nome', 'Motivo']
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 		writer.writeheader()
-		writer.writerow({'Inizio': '1230', 'Fine': '1300', 'Nome': 'Barabba', 'Motivo': 'Sì'})
 	return
-	
-#Creatabella()
-
-
-inizio = datetime(2021, 2, 12, 13,45)
-fine = datetime(2021, 2, 12, 14,55)
-nome = 'PITUCCI'
-motivo = 'crack'
-tuplaesempio = (inizio, fine, nome, motivo)
-
-#print(data.year)
-#print(data.strftime(FORMAT))
 
 
 def AggiungiPrenotazione(tup):
@@ -35,12 +35,45 @@ def AggiungiPrenotazione(tup):
 	Fine = tup[1].strftime(FORMAT)
 	Nome = tup[2]
 	Motivo = tup[3]
-	with open(FILENAME, 'a', newline='') as csvfile:
+	with open(FILENAME, 'a') as csvfile:
 		fieldnames = ['Inizio', 'Fine', 'Nome', 'Motivo']
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-		writer.writerow({'Inizio': Inizio, 'Fine': Fine, 'Nome': Nome, 'Motivo': Motivo})
-	
+		writer.writerow({'Inizio': Inizio, 'Fine': Fine, 'Nome': Nome, 'Motivo': Motivo,})
 	return
+
+
+
+
+def AggiungiEventi(calendario):
+    """calendario deve essere una lista di eventi in formato standard (tupla etc.) scrive un csv"""
+    Creatabella(FILENAME)
+    for evento in calendario:
+        # print(evento)
+        AggiungiPrenotazione(evento)
+        
+
+
 	
-	
+
+
+inizio = datetime(2021, 2, 12, 13,45)
+fine = datetime(2021, 2, 12, 14,55)
+nome = 'PITUCCI'
+motivo = 'CRACK'
+tuplaesempio = (inizio, fine, nome, motivo)
+
+#print(data.year)
+#print(data.strftime(FORMAT))
+
+# AggiungiEventi([])
 AggiungiPrenotazione(tuplaesempio)
+
+A = PrendiCalendario()
+
+# print(A)	
+
+AggiungiEventi(A)
+
+
+
+
