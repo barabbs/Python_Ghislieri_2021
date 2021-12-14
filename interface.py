f4983f05c361a7aa457fbab2f4349ac843db99f7
Romeo Gessaga <you@example.com>
2021-12-14 22:39:48 +0100

    aggiunto stampa calendario

--- interface.py
+++ interface.py
@@ -1,61 +1,58 @@
 from datetime import datetime
-from checks import Controllo, Aggiungi
+from checks import Controllo, Aggiungi, StampaCalendario
 
+def input_data_ora():
+    while True:    
+        try:
+            scelta_anno = int(input("anno"))
+            scelta_mese = int(input ("mese"))
+            scelta_giorno = int(input("giorno"))
+            scelta_ora_inizio = int(input ("ora inizio"))
+            scelta_minuto_inizio = int(input ("minuto inizio")) 
+            scelta_ora_fine = int(input ("ora fine")) 
+            scelta_minuto_fine = int(input ("minuto fine"))
+            
+            iniziale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_inizio, scelta_minuto_inizio)
+            finale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_fine, scelta_minuto_fine)
+            break
+        except ValueError:
+            print("inserisci la data nel formato AAAA-MM-GG e l'ora nel formato HH-MM")
+    return iniziale, finale
+
+def input_data():
+    while True:    
+        try:
+            scelta_anno = int(input("anno"))
+            scelta_mese = int(input ("mese"))
+            scelta_giorno = int(input("giorno"))
+            
+            data= datetime(scelta_anno, scelta_mese, scelta_giorno)            
+            break
+        except ValueError:
+            print("inserisci la data nel formato AAAA-MM-GG e l'ora nel formato HH-MM")
+    return data
+    
 while True:
-        scelta = input("cosa vuoi fare?\n a)prenotare campo\n b)controllare orari\n q)esci\n")
+        scelta = input("cosa vuoi fare?\n a)prenotare campo\n b)controllare orari\n c)visualizza calendario\n q)esci\n").lower()
         
         if scelta == "q":
             break
         
-        if scelta == "a": 
+        elif scelta == "a": 
             print("Per prenotare a cavallo di due giorni, fai due prenotazioni per i due giorni diversi")
             nome = input("come ti chiami?")
             motivo = input("motivo della prenotazione?") 
-            
-            while True:    
-                try:
-                    scelta_anno = int(input("anno"))
-                    scelta_mese = int(input ("mese"))
-                    scelta_giorno = int(input("giorno"))
-                    scelta_ora_inizio = int(input ("ora inizio"))
-                    scelta_minuto_inizio = int(input ("minuto inizio")) 
-                    scelta_ora_fine = int(input ("ora fine")) 
-                    scelta_minuto_fine = int(input ("minuto fine"))
-                    
-                    iniziale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_inizio, scelta_minuto_inizio)
-                    finale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_fine, scelta_minuto_fine)
-                    break
-                except ValueError:
-                    print("inserisci la data nel formato AAAA-MM-GG e l'ora nel formato HH-MM")
-                    
-                    
-                    
+            iniziale,finale=input_data_ora()
             x = Aggiungi(iniziale, finale, nome, motivo)
             print(x)
             
-        if scelta == "b":
-            while True:    
-                try:
-                    scelta_anno = int(input("anno"))
-                    scelta_mese = int(input ("mese"))
-                    scelta_giorno = int(input("giorno"))
-                    scelta_ora_inizio = int(input ("ora inizio"))
-                    scelta_minuto_inizio = int(input ("minuto inizio")) 
-                    scelta_ora_fine = int(input ("ora fine")) 
-                    scelta_minuto_fine = int(input ("minuto fine"))
-                    
-                    iniziale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_inizio, scelta_minuto_inizio)
-                    finale= datetime(scelta_anno, scelta_mese, scelta_giorno, scelta_ora_fine, scelta_minuto_fine)
-                    break
-                except ValueError:
-                    print("inserisci la data nel formato AAAA-MM-GG e l'ora nel formato HH-MM")
-            
-            
+        elif scelta == "b":
+            iniziale,finale=input_data_ora()
             x = Controllo(iniziale, finale)
             if x == True:
                 print("è libero")
             else:
                 print ("non è libero")
-        
-    
-        
+        elif scelta == "c":
+            data=input_data()
+            StampaCalendario(data)
